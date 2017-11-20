1ddd28d18dc6c7e1352f024b21b5beae5d45e5e0
Julius Gonsior <julius@gaenseri.ch>
2017-11-20 17:16:04 +0100

    fix empty page bug?

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  window.sr = ScrollReveal( {
-    reset: true,
-    opacity: 0.2,
-    duration: 500,
-    mobile: false
-  });
-
-  sr.reveal('.background');
-
-  sr.reveal('section');
-  sr.reveal('#a1 h2', 50);
-
-  sr.reveal('#contacts li', 50);
+window.sr = ScrollReveal( {
+  reset: true,
+  opacity: 0.2,
+  duration: 500,
+  mobile: false
 });
+
+sr.reveal('.background');
+
+sr.reveal('section');
+sr.reveal('#a1 h2', 50);
+
+sr.reveal('#contacts li', 50);
