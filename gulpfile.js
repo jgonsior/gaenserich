4c152c0d97835a6742a109cc6056128eae0c1214
Julius Gonsior <julius@gaenseri.ch>
2018-05-26 15:21:58 +0200

    Updated gulp to v4, font awesome to v5 und added noto font as npm package

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,57 @@
 'use strict';
+
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
-var cssnano = require('gulp-cssnano');
-var htmlMinifier = require('gulp-html-minifier');
-var del = require('del');
-var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var cssnano = require('gulp-cssnano');
 
-gulp.task('views', () => {
+
+gulp.task('html', function() {
     return gulp.src('src/pug/*.pug')
-        .pipe(pug({}))
-        .pipe(gulp.dest('dist/'));
+        .pipe(pug())
+        .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('html', () => {
-    return gulp.src('src/pug/*.pug')
-        .pipe(pug({}))
-        .pipe(htmlMinifier({
-            removeComments: true,
-            collapseWhitespace: true,
-            removeTagWhitespace: true
-        }))
-        .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('copy', () => {
-    return gulp.src('src/dist/**/*')
-        .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('scripts', function() {
-    return gulp.src(['node_modules/scrollreveal/dist/scrollreveal.min.js', 'src/js/main.js'])
-        .pipe(concat('script.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/'));
-
-})
-
-gulp.task('copy-watch', ['copy'], function(done) {
-    browserSync.reload();
-    done();
-})
-
-gulp.task('fonts', function() {
-    return gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('dist/fonts'))
-})
-
-gulp.task('css', () => {
+gulp.task('css', function() {
     return gulp.src(['node_modules/normalize.css/normalize.css', 'src/less/*.less'])
         .pipe(concat('style.css'))
         .pipe(less())
         .pipe(cssnano({
             preset: 'advanced'
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('clean', (cb) => {
-    return del.sync('dist', cb);
+gulp.task('copy', () => {
+    return gulp.src('src/dist/**/*')
+        .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('watch', () => {
-    return gulp.watch('src/**/*', ['views', 'scripts', 'css', 'copy']);
+gulp.task('fonts', function() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/webfonts/*').pipe(gulp.dest('dist/webfonts'))
 });
 
-gulp.task('build', ['views', 'fonts', 'scripts', 'css', 'copy']);
+gulp.task('js', function() {
+    return gulp.src(['node_modules/scrollreveal/dist/scrollreveal.js', 'src/js/main.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('script.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/js'))
+});
 
-gulp.task('cleanandbuild', ['clean', 'build']);
 
-gulp.task('default', ['cleanandbuild']);
-
-// Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./dist/"
+            baseDir: "./dist"
         }
     });
 
-    gulp.watch('src/**/*', ['views', 'scripts', 'css', 'copy-watch']);
-
+    gulp.watch("src/**/*", ['html', 'css', 'js', 'fonts', 'copy']);
+    gulp.watch("src/**/*").on('change', browserSync.reload);
 });
+
+gulp.task('default', ['html', 'css', 'js', 'fonts', 'copy']);
---
+++ gulpfile_old.js
@@ -0,0 +1,86 @@
+'use strict';
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var less = require('gulp-less');
+var cssnano = require('gulp-cssnano');
+var htmlMinifier = require('gulp-html-minifier');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('views', () => {
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({}))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({}))
+        .pipe(htmlMinifier({
+            removeComments: true,
+            collapseWhitespace: true,
+            removeTagWhitespace: true
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy', () => {
+    return gulp.src('src/dist/**/*')
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(['node_modules/scrollreveal/dist/scrollreveal.min.js', 'src/js/main.js'])
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+
+})
+
+gulp.task('copy-watch', ['copy'], function(done) {
+    browserSync.reload();
+    done();
+})
+
+gulp.task('fonts', function() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/webfonts/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('css', () => {
+    return gulp.src(['node_modules/normalize.css/normalize.css', 'src/less/*.less'])
+        .pipe(concat('style.css'))
+        .pipe(less())
+        .pipe(cssnano({
+            preset: 'advanced'
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('clean', (cb) => {
+    return del.sync('dist', cb);
+});
+
+gulp.task('watch', () => {
+    return gulp.watch('src/**/*', ['views', 'scripts', 'css', 'copy']);
+});
+
+gulp.task('build', ['views', 'fonts', 'scripts', 'css', 'copy']);
+
+gulp.task('cleanandbuild', ['clean', 'build']);
+
+gulp.task('default', ['cleanandbuild']);
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+
+    gulp.watch('src/**/*', ['views', 'scripts', 'css', 'copy-watch']);
+
+});
